7884fcc10168ba439547ddeea8ec948a63ee8476
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-06-19 21:16:43 -0300

    feat table: create service

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get, Post } from '@nestjs/common'; // this is importing the methods used to render the data
+import { TableService } from './table.service'; // this is importing the functions from table.service as methods
 
 @Controller('table') //using the table here you are setting the table as the prefix for this controller
 export class TableController {
+  constructor(private tableService: TableService) {} // this is creating in the class as the object tableService with the methods from the imported TableSevice as it's contents. and it's declared as private so it only exists inside of this class.
+
   //check this out, now you can do the get, post, put and delete easy.
   @Get() //using the @get to find all the tables
   findAll() {
-    return 'Buscar todas as mesas';
+    return this.tableService.findAll();
   }
 
   @Post() //using the @post to insert a new table
   create() {
-    return 'Criar uma mesa';
+    return this.tableService.create();
   }
 }
--- src/table/table.module.ts
+++ src/table/table.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TableController } from './table.controller';
+import { TableService } from './table.service';
 
 @Module({
   controllers: [TableController],
-  providers: [],
+  providers: [TableService], // using the TableService as a provider, it makes it possible to send it's methods into a controller, in this case a TableController
 })
 export class TableModule {}
