5ea1267b39c04e4c13bcb1b234ef3381e81c6ff6
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-06-19 23:18:33 -0300

    feat add tables and data source to service

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -4,17 +4,17 @@ import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
-  tables: Table[] = [];
+  tables: Table[] = []; // this is creating the item tables in this class with the data pulled from the table.entity
 
   findAll() {
-    return this.tables;
+    return this.tables; //return the item tables created in the first line of this class
   }
 
   create(createTableDto: CreateTableDto) {
-    const table: Table = { id: 'random_id', ...createTableDto };
+    const table: Table = { id: 'random_id', ...createTableDto }; //this creates an item called table that has an id and all the info from the createTableDto using the spread(...) operator
 
-    this.tables.push(table);
+    this.tables.push(table); //this inserts the item table into the tables
 
-    return table;
+    return table; // this returns the item table
   }
 }
