e2f6ebe94c44c3c1520d6c57e171ee8ece667f1d
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-06-29 00:46:37 -0300

    feat(app): add swagger doc

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'; // this is importing the methods used to render the data
+import { Controller, Get, Post, Body, Param } from '@nestjs/common'; // this is importing the methods used to render the data
 import { TableService } from './table.service'; // this is importing the functions from table.service as methods
 import { CreateTableDto } from './dto/create-table.dto'; // this is importing the functions from create-table.dto as methods
-import { ApiTags } from '@nestjs/swagger'; // this is importing from the swagger for apiTags (must install with npm first)
+import { ApiOperation, ApiTags } from '@nestjs/swagger'; // this is importing from the swagger for apiTags (must install with npm first)
+import { Table } from './entities/table.entity';
 
 @ApiTags('table') // links the controller to the all the APITAGs (get, post, delete, etc)
 @Controller('table') //using the table here you are setting the table as the prefix for this controller
@@ -10,12 +11,26 @@ export class TableController {
 
   //check this out, now you can do the get, post, put and delete easy.
   @Get() //using the @get to find all the tables
-  findAll() {
+  @ApiOperation({
+    summary: 'List all the tables',
+  })
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
 
+  @Get(':id') // using the :id is to define that the parameter id is to be brought into this property as a variable
+  @ApiOperation({
+    summary: 'Show a table',
+  })
+  findOne(@Param('id') id: string): Promise<Table> { // the @Param must be included to define the id as parameter of search.
+    return this.tableService.findOne(id);
+  }
+
   @Post() //using the @post to insert a new table
-  create(@Body() dto: CreateTableDto) {
+  @ApiOperation({
+    summary: 'Create a table',
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
     // with this @body it now is getting the info from createTableDto and inserting into the @body
     return this.tableService.create(dto);
   }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -7,11 +7,15 @@ import { Table } from './entities/table.entity';
 export class TableService {
   constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
+  findAll(): Promise<Table[]> {
     return this.prisma.table.findMany(); //return all the items in the prisma.table
   }
 
-  create(dto: CreateTableDto) {
+  findOne(id: string): Promise<Table> {
+    return this.prisma.table.findUnique({ where: { id }}); //return the item where the value id is equal to the one informed from the prisma.table
+  }
+
+  create(dto: CreateTableDto): Promise<Table> {
     const data: Table = { ...dto }; //this creates an item called table that has an id(as optional in the entity) and all the info from the createTableDto using the spread(...) operator. If you name the receiving as data, you can just use the data in the create method, since the key and value is of the same name
 
     return this.prisma.table.create({ data }); // this returns the new data table(since it has the name data you can just write data instead of data:data) to the prisma table with the id getting created automatically with the ORM.
