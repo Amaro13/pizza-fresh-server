437fe2fb950fa7aeb8b8a306c3dc2f810c85e6d3
Rikuster13 <amaro_ribeir0@hotmail.com>
2022-07-06 00:06:31 -0300

    feat(user) using bcrypt to encrypt and add safety features

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,22 +2,39 @@ import {
   Injectable,
   NotFoundException,
   UnprocessableEntityException,
+  BadRequestException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    nickname: true,
+    name: true,
+    password: false,
+    image: true,
+    createdAt: true,
+    updatedAt: true,
+  }; //this selects the patter in the constructor below what is to be returned when searched. this is the info that can be taken from the database is as true, but what can't be taken is as false. Notice how the userSelect is used in the code below for further undestanding.
+
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<User[]> {
-    return this.prisma.user.findMany(); //return all the items in the prisma.user
+    return this.prisma.user.findMany({
+      select: this.userSelect,
+    }); //return all the items in the prisma.user
   }
 
   async findById(id: string): Promise<User> {
-    const record = await this.prisma.user.findUnique({ where: { id } }); //return the item where the value id is equal to the one informed from the prisma.user
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    }); //return the item where the value id is equal to the one informed from the prisma.user
 
     if (!record) {
       throw new NotFoundException(`Register with the id:'${id}' not found.`);
@@ -30,17 +47,39 @@ export class UserService {
     return this.findById(id);
   }
 
-  create(dto: CreateUserDto): Promise<User> {
-    const data: User = { ...dto }; //this creates an item called user that has an id(as optional in the entity) and all the info from the createUserDto using the spread(...) operator. If you name the receiving as data, you can just use the data in the create method, since the key and value is of the same name
+  async create(dto: CreateUserDto): Promise<User> {
+    if (dto.password != dto.confirmPassword) {
+      //it's checking here if the password is correct
+      throw new BadRequestException('The password is incorrect.'); // if it's incorrect, this returns.
+    }
 
-    return this.prisma.user.create({ data }).catch(this.handleError); // this returns the new data user(since it has the name data you can just write data instead of data:data) to the prisma user with the id getting created automatically with the ORM. If an error occurs it prints the error and returns undefined.
+    delete dto.confirmPassword; // this serves to delete an item from the dto, so deleting the confirmPassword.
+    const data: User = {
+      ...dto,
+      password: await bcrypt.hash(dto.password, 10), // this is how you use bcrypt, using await and bcrypt.hash(password), the number is unecessary as the number of rounds needed to run is by default the 10, but hey, you wanna be a know it all...
+    }; //this creates an item called user that has an id(as optional in the entity) and all the info from the createUserDto using the spread(...) operator. If you name the receiving as data, you can just use the data in the create method, since the key and value is of the same name
+
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(this.handleError); // this returns the new data user(since it has the name data you can just write data instead of data:data) to the prisma user with the id getting created automatically with the ORM. If an error occurs it prints the error and returns undefined.
   }
 
   async update(id: string, dto: UpdateUserDto): Promise<User> {
     await this.findById(id);
+    if (dto.password) {//checks if the dto you are using has a password
+      if ((dto.password != dto.confirmPassword)) { // if it has a password is it correct?
+        throw new BadRequestException('The passwords are not the same.');
+      }
+    }
+
+    delete dto.confirmPassword; //deletes the need to confirm it again
+
     const data: Partial<User> = { ...dto }; // here you are using the Partial <> to turn every value within, in this case the user, into optional values
 
-    return this.prisma.user.update({
+    if (data.password) { //encrypts the password
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+    return this.prisma.user.update({ //updates the item where the id is the same with the info included in data
       where: { id },
       data,
     });
@@ -54,6 +93,11 @@ export class UserService {
   handleError(error: Error): undefined {
     const errorLines = error.message?.split('\n');
     const lastErrorLine = errorLines[errorLines.length - 1]?.trim();
+
+    if (!lastErrorLine) {
+      console.error(error);
+    }
+
     throw new UnprocessableEntityException(
       lastErrorLine || 'Some undefined error occurred',
     ); // this is magic, if it fails to proccess in any case, it returns the error
